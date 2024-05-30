9f040e3cb1fe1e75bd2238854f7e24794ce96b6b
yuxi Shi <ys3599@tc.columbia.edu>
2024-05-30 01:09:31 -0400

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+  
+    for (let n = 0; n < names.length; n++) {
+      const name = names[n];
+      const message = `Thank you, ${name}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+}
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
